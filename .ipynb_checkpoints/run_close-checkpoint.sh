473ca02111bccf209b22d816c0d3d871feec08a6
Dimitrios Georgiou <dgeorgiou3@gmail.com>
2024-09-24 00:54:07 +0200

    Initial Commit v3

--- .ipynb_checkpoints/run_close-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-source /home/$(whoami)/$(whoami)_nfs/production/krakow_ops_enablement/EMEA_EATS/dimitrios/churn_program/venv/bin/activate
-cd     /home/$(whoami)/$(whoami)_nfs/production/krakow_ops_enablement/EMEA_EATS/dimitrios/churn_program
-python3 main_churnV3.py --operation close_cases
-
