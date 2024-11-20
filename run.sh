de5680dd4fb733cbd6d4fc856623a4214abcadf8
Dimitrios Georgiou <dgeorgiou3@gmail.com>
2024-11-20 13:36:24 +0100

    (Telegram API) Refresh Google Token

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@
 #source venv/bin/activate
 
 # Run the Python script
-python3 main.py --schedule_hour 6 --hours 30 --enable_polling 
+python3 main.py --schedule_hour 6 --hours 35 --enable_polling 
 
 # Deactivate the virtual environment
 #deactivate
